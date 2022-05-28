6b579bcec68ca506bd6d0de14b62d0ac7892f9c8
joanplanas7 <72151839+joanplanas7@users.noreply.github.com>
2022-05-28 17:22:30 +0200

    botones cambiar idoma añadidos

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ const navbar = document.querySelector('.movilidad');
 window.addEventListener('scroll', function(){
     navbar.classList.toggle('active', this.window.scrollY > 0);
 });
+
+
+//cambiar icono cuando se pulsa la bandera
+const butCat = document.getElementById("cat");
+const butEsp = document.getElementById("esp");
+
+butCat.addEventListener('click', ()=>{
+    butCat.classList.add("languageActive");
+    butEsp.classList.remove("languageActive");
+});
+
+butEsp.addEventListener('click', ()=>{
+    butEsp.classList.add("languageActive");
+    butCat.classList.remove("languageActive");
+});
+
